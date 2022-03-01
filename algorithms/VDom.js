4fc919e2af52cf6e115f4c3ec57e932360d02584
kartjim <jjack_chen3@163.com>
2022-03-01 21:56:20 +0800

    虚拟DOM

---
+++ algorithms/VDom.js
@@ -0,0 +1,56 @@
+/**
+ * 渲染虚拟dom
+ */
+// 虚拟DOM
+const vdom = {
+    tag: "div",
+    events: {
+        onDblClick: () => console.log('虚拟DOM - div'),
+    },
+    props: {
+        style: 'padding: 10px;margin: 50px;width: 200px;background: #eee;'
+    },
+    children: [
+        {
+            tag: "a",
+            events: {
+                onClick: () => confirm('虚拟DOM'),
+            },
+            props: {
+                href: '#',
+                style: 'background: #dfdfdf;'
+            },
+            children: "click me"
+        },
+        {
+            tag: "span",
+            children: "span"
+        }
+    ]
+}
+// 渲染函数
+function render(vdom, container){
+    const tag = document.createElement(vdom.tag);
+    // events
+    for(let event in vdom.events){
+        if(/^on/.test(event)){
+            tag.addEventListener(
+                event.substring(2).toLowerCase(),
+                vdom.events[event]
+            )
+        }
+    }
+    // props
+    for(let prop in vdom.props){
+        tag.setAttribute(prop,vdom.props[prop])
+    }
+    if(typeof vdom.children === 'string'){
+        tag.appendChild(document.createTextNode(vdom.children));
+    } else if(Array.isArray(vdom.children)){
+        vdom.children.forEach(function (child){
+            render(child,tag);
+        });
+    }
+    container.appendChild(tag);
+}
+render(vdom,document.body);
